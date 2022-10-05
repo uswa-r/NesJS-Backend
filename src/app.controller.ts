5748430e3ab9a60b7c1afb7c1a868afc743cd49d
uswa.r <uswa.r@outlook.com>
2022-10-06 00:00:33 +0500

    i

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-ii
+iii
   @Get()
   getHello(): string {
     return this.appService.getHello();
