571a7aee0557dbec9c71994be4ea1cd55f144902
uswa.r <uswa.r@outlook.com>
2022-10-09 02:03:07 +0500

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarsResolver } from './cars/cars.resolver';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CarsResolver],
 })
 export class AppModule {}
